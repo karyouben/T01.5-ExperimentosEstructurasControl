392c9b1dd6e2ea529b2e6b5909cdfb694a4f5db7
willi <willi@DESKTOP-K0BDO30>
2022-02-24 17:33:26 +0100

    24/02/2022

--- src/fp/experimentos/ExperimentosEstructrurasControl.java
+++ src/fp/experimentos/ExperimentosEstructrurasControl.java
@@ -17,7 +17,7 @@ public class ExperimentosEstructrurasControl {
 	 * Muestra por consola un mensaje que indica si n es par.
 	 */
 	public static void mensajePar (Integer n) {
-		String s = String.format("El numero %d es ", n);
+		String s = String.format("El numero %d es: ", n);
 		System.out.println(s);
 		//System.out.println("El número es " + n + " es ");
 		if (n % 2 == 0) {
@@ -26,6 +26,21 @@ public class ExperimentosEstructrurasControl {
 				System.out.println("impar");
 			}
 		}
+	public static Float calcularImpuesto(Float salario) {
+		Float impuesto = 0.0f;
+		if (salario>= 5000.0) {
+			impuesto = 20.0f;
+		}else if (salario < 5000.0 && salario >= 2500.0) {
+			impuesto = 15.0f;	
+		}else if (salario < 2500.0 && salario >= 1500.0) {
+			impuesto = 10.0f;
+		}else if (salario > 800) {
+			impuesto = 5.0f;
+		//Float porcentaje = (salario*impuesto)/100;
+	  }
+		return impuesto;
+	
+	}
 }		
    
 
--- src/fp/experimentos/test/TestExperimentosEstructurasControl.java
+++ src/fp/experimentos/test/TestExperimentosEstructurasControl.java
@@ -1,12 +1,63 @@
 package fp.experimentos.test;
 
+import java.util.Scanner;
+
 import fp.experimentos.ExperimentosEstructrurasControl;
 
 public class TestExperimentosEstructurasControl {
 
 	public static void main(String[] args) {
-		ExperimentosEstructrurasControl.mensajePar(4);
-
+		
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.println("Introduzca un número entero : ");
+		Integer n = leeEntero(sc, "Introduzca un número entero :");
+		//Integer n = sc.nextInt();
+		ExperimentosEstructrurasControl.mensajePar(n);
+		//ExperimentosEstructrurasControl.mensajePar(3);
+		System.out.println("Introduzca su salario: ");
+		Float salario = sc.nextFloat();
+		Float impuesto = ExperimentosEstructrurasControl.calcularImpuesto(salario);
+		String s = String.format("El impuesto correspondiente al salario %.2f es %.2f", salario, impuesto);
+		System.out.println(s);
+		sc.close();
+	}
+	
+	
+	public static Integer leeEntero(Scanner sc, String mensaje) {
+		//System.out.println("Introduzca un número entero : ");
+		Integer n = sc.nextInt();
+		return n;
+	}
+	
+	public static String obtenNombreDia(Integer n) {
+		String res = "" ;
+        switch (n){
+        case 1:              
+            res = "Lunes";
+            break;
+        case 2:              
+        	res = "Martes";
+            break;
+        case 3:             
+        	res = "Miercoles";
+            break;
+        case 4:             
+        	res = "Jueves";
+            break;
+        case 5:             
+        	res = "Viernes";
+            break;           
+        case 6: 
+        	res = "Sabado";
+            break;
+        case 7: 
+        	res = "Domingo";
+            break;
+                  
+      }
+       return res;
+       
 	}
 
 }
